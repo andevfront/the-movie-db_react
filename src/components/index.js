fcfcb00c282c21db3f927ed26524d4052b92eb3b
PabloAndrade96 <pablopopandrade@outlook.com>
2023-07-29 18:36:37 -0300

    paginacion implementada

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export * from "./Search";
 export * from "./SearchResults";
 export * from "./modal";
 export * from "./loadingButton";
+export * from "./pagination";
--- src/pages/home/components/MovieCollection.jsx
+++ src/pages/home/components/MovieCollection.jsx
@@ -1,16 +1,28 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import { MovieCard } from "./MovieCard";
+import { useFetch } from "../../../hooks";
+import { Pagination } from "../../../components";
 
 export const MovieCollection = () => {
-  const { popularMovies } = useLoaderData();
+  const [page, setPage] = useState(1);
 
-  const { results } = popularMovies;
+  const key = import.meta.env.VITE_API_KEY;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&page=${page}`;
+
+  const { data, isLoading } = useFetch(url);
+
+  if (isLoading) return;
+
+  const { results, total_pages } = data;
 
   return (
-    <div className="grid grid-cols-12 gap-5">
-      {results.map((item) => (
-        <MovieCard key={item.id} {...item} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-12 gap-5">
+        {results.map((item) => (
+          <MovieCard key={item.id} {...item} />
+        ))}
+      </div>
+      <Pagination setPage={setPage} totalPages={total_pages} />
+    </>
   );
 };
